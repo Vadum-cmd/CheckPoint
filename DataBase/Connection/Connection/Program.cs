01da759383ea8f54b2acabfa6516d97201a84e3f
Rednigo <barostikq@gmail.com>
2023-10-19 22:40:12 +0300

    Connected to data base with ADO.Net

---
+++ DataBase/Connection/Connection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Connection
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string theConnectionString = ConfigurationManager.ConnectionStrings["MainConnection"].ToString();
+            MySqlConnection connection = new MySqlConnection(theConnectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected to database.");
+
+                // Perform your database operations here
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
