ba962089768d85b9d0add65a65748fd096975609
OleksandrVlasiuk <alexvlasyk31@gmail.com>
2023-11-15 16:53:46 +0200

    Intellis cracked

---
+++ App/Presentation/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,3 @@
+﻿public class CustomerViewModel : ViewModelBase
+{
+}
--- App/Presentation/ViewModels/MainViewModel.cs
+++ App/Presentation/ViewModels/MainViewModel.cs
@@ -1,102 +1,103 @@
 ﻿using FontAwesome.Sharp;
 using System.Windows.Input;
 
-//public class MainViewModel : ViewModelBase
-//{
-//    //Fields
-//    private UserAccountModel _currentUserAccount;
-//    private ViewModelBase _currentChildView;
-//    private string _caption;
-//    private IconChar _icon;
+public class MainViewModel : ViewModelBase
+{
+    //Fields
+    //private UserAccountModel _currentUserAccount;
+    //private ViewModelBase _currentChildView;
+    //private string _caption;
+    //private IconChar _icon;
 
-//    private IUserRepository userRepository;
+    //private IUserRepository userRepository;
 
-//    //Properties
-//    public UserAccountModel CurrentUserAccount
-//    {
-//        get
-//        {
-//            return _currentUserAccount;
-//        }
+    ////Properties
+    //public UserAccountModel CurrentUserAccount
+    //{
+    //    get
+    //    {
+    //        return _currentUserAccount;
+    //    }
 
-//        set
-//        {
-//            _currentUserAccount = value;
-//            OnPropertyChanged(nameof(CurrentUserAccount));
-//        }
-//    }
+    //    set
+    //    {
+    //        _currentUserAccount = value;
+    //        OnPropertyChanged(nameof(CurrentUserAccount));
+    //    }
+    //}
 
-//    public ViewModelBase CurrentChildView
-//    {
-//        get
-//        {
-//            return _currentChildView;
-//        }
+    //public ViewModelBase CurrentChildView
+    //{
+    //    get
+    //    {
+    //        return _currentChildView;
+    //    }
 
-//        set
-//        {
-//            _currentChildView = value;
-//            OnPropertyChanged(nameof(CurrentChildView));
-//        }
-//    }
+    //    set
+    //    {
+    //        _currentChildView = value;
+    //        OnPropertyChanged(nameof(CurrentChildView));
+    //    }
+    //}
 
-//    public string Caption
-//    {
-//        get
-//        {
-//            return _caption;
-//        }
+    //public string Caption
+    //{
+    //    get
+    //    {
+    //        return _caption;
+    //    }
 
-//        set
-//        {
-//            _caption = value;
-//            OnPropertyChanged(nameof(Caption));
-//        }
-//    }
+    //    set
+    //    {
+    //        _caption = value;
+    //        OnPropertyChanged(nameof(Caption));
+    //    }
+    //}
 
-//    public IconChar Icon
-//    {
-//        get
-//        {
-//            return _icon;
-//        }
+    //public IconChar Icon
+    //{
+    //    get
+    //    {
+    //        return _icon;
+    //    }
 
-//        set
-//        {
-//            _icon = value;
-//            OnPropertyChanged(nameof(Icon));
-//        }
-//    }
+    //    set
+    //    {
+    //        _icon = value;
+    //        OnPropertyChanged(nameof(Icon));
+    //    }
+    //}
 
-//    //--> Commands
-//    public ICommand ShowHomeViewCommand { get; }
-//    public ICommand ShowCustomerViewCommand { get; }
+    ////--> Commands
+    //public ICommand ShowHomeViewCommand { get; }
+    //public ICommand ShowCustomerViewCommand { get; }
 
-//    public MainViewModel()
-//    {
-//        userRepository = new UserRepository();
-//        CurrentUserAccount = new UserAccountModel();
+    //public MainViewModel()
+    //{
+    //    userRepository = new UserRepository();
+    //    CurrentUserAccount = new UserAccountModel();
 
-//        //Initialize commands
-//        ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
-//        ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+    //    //Initialize commands
+    //    ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+    //    ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
 
-//        //Default view
-//        ExecuteShowHomeViewCommand(null);
+    //    //Default view
+    //    ExecuteShowHomeViewCommand(null);
 
-//        LoadCurrentUserData();
-//    }
+    //    LoadCurrentUserData();
+    //}
 
-//    private void ExecuteShowCustomerViewCommand(object obj)
-//    {
-//        CurrentChildView = new CustomerViewModel();
-//        Caption = "Customers";
-//        Icon = IconChar.UserGroup;
-//    }
+    //private void ExecuteShowCustomerViewCommand(object obj)
+    //{
+    //    CurrentChildView = new CustomerViewModel();
+    //    Caption = "Customers";
+    //    Icon = IconChar.UserGroup;
+    //}
 
-//    private void ExecuteShowHomeViewCommand(object obj)
-//    {
-//        CurrentChildView = new HomeViewModel();
-//        Caption = "Dashboard";
-//        Icon = IconChar.Home;
-//    }
+    //private void ExecuteShowHomeViewCommand(object obj)
+    //{
+    //    CurrentChildView = new HomeViewModel();
+    //    Caption = "Dashboard";
+    //    Icon = IconChar.Home;
+    //}
+}
