6d3c35476282d0ce7376663320ddb925243f0998
Rednigo <barostikq@gmail.com>
2023-11-24 07:35:55 +0200

    Merging

--- App/Presentation/obj/Debug/net7.0-windows/Presentation.AssemblyInfo.cs
+++ App/Presentation/obj/Debug/net7.0-windows/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ddd5d05584255dddc3cc706bfad4b9cd84a0ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
