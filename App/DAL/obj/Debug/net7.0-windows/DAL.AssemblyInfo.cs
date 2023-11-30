a0cf131363f8ecbc226072b8fdbb9dae41a76d3e
Rednigo <barostikq@gmail.com>
2023-11-30 19:26:57 +0200

    changed

--- App/DAL/obj/Debug/net7.0-windows/DAL.AssemblyInfo.cs
+++ App/DAL/obj/Debug/net7.0-windows/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a273ee4177548882280b5c8ad55562968f9b5451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
